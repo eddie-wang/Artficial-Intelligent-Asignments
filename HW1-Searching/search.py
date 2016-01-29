1134dfdbe0a0955132d6a3d3373adf94d473edca
eddie <786830875@qq.com>
2016-01-29 13:24:47 -0800

    fix greddyBFS

--- HW1-Searching/search.py
+++ HW1-Searching/search.py
@@ -1,9 +1,10 @@
+import sys
 class Game:
     def __init__(self,inputFile):
         with open(inputFile) as f:
             self.initGame(f)
         if self.task!=4:
-            self.nextState(self.player,self.task,self.cutff)
+            self.nextState(self.firstPlayer,self.task,self.cutOff)
         else:
             self.play()
     def initGame(self,file):
@@ -11,46 +12,49 @@ class Game:
         self.task=int(file.readline())
         self.firstPlayerValue=0
         self.secondPlayerValue=0
-        if task!=4:
-            self.player=file.readline()
-            self.cutoff=int(file.readline())
+        if self.task!=4:
+            self.firstPlayer=file.readline().strip()
+            self.secondPlayer="X" if self.firstPlayer=="O" else "O"
+            self.cutOff=int(file.readline().strip())
             ## outputfile
             self.nextStateOuput=open("next_state.tx","w")
-            if task!=1:
+            if self.task!=1:
                 self.traverseOut=open("traverse_log.txt","w")
         else:
-            self.firstPlayer=file.readline()
-            self.firstAlg=int(file.readline())
-            self.firstCutOff=int(file.readline())
-            self.secondPlayer=file.readline()
-            self.secondAlg=int(file.readline())
-            self.secondCutOff=int(file.readline())
+            self.firstPlayer=file.readline().strip()
+            self.firstAlg=int(file.readline().strip())
+            self.firstCutOff=int(file.readline().strip())
+            self.secondPlayer=file.readline().strip()
+            self.secondAlg=int(file.readline().strip())
+            self.secondCutOff=int(file.readline().strip())
             self.traceOuput=open("trace_state.txt","w")
-        self.boardValue=[[]*5] ##int
+        self.boardValue=[] ##int
         for line in range(5):
+            self.boardValue.append([])
             for v in file.readline().split():
-                self.boardValue[line].append(v)
-        self.boardState[[]*5] ##char
-        for line in range(5):
-            for index,v in enumerate(file.readline().split()):
-                self.boardState[line].append(v)
+                self.boardValue[line].append(int(v))
+        print self.boardValue
+        self.boardState=[] ##char
+        for index,line in enumerate(file.readlines()):
+            self.boardState.append(list(line.strip()))
+            for i,v in enumerate(line.strip()):
                 if v==self.firstPlayer:
-                    self.firstPlayerValue+=self.boardValue[line][index]
+                    self.firstPlayerValue+=self.boardValue[index][i]
                 elif v==self.secondPlayer:
-                    self.secondPlayerValue+=self.boardValue[line][index]
-
+                    self.secondPlayerValue+=self.boardValue[index][i]
+        print self.boardState
     def play(self):
         while self.gameEnd==False:
             nextState(self.firstPlayer,self.firstAlg,self.firstCutOff)
             nextState(self.secondPlayer,self.secondAlg,self.secondCutOff)
     def  nextState(self,player,task,cutoff):
-        methodDict={1:greedyBFS,2:minMax,3:alphaPruning}
+        methodDict={1:self.greedyBFS,2:self.minMax,3:self.alphaPruning}
         methodDict[task](player,cutoff)
-        printState()
+        self.printState()
 
-    def move(self,player,result):
+    def move(self,player,i,j,res):
         ## can reduce code by using dict modify!!
-
+        print i,j,"wxg",res
         if player==self.firstPlayer:
             self.firstPlayerValue=res[0]
             self.secondPlayerValue=res[1]
@@ -59,9 +63,9 @@ class Game:
             self.firstPlayerValue=res[0]
         self.boardState[i][j]=player
 
-        direction=[(1:1),(1:-1),(-1,1),(-1,-1)]
+        direction=[(0,1),(1,0),(-1,0),(0,-1)]
 
-        for d in result[2]:
+        for d in res[3]:
             self.boardState[i+direction[d][0]][j+direction[d][1]]=player
 
     def evaluation(self,player,pos_i,pos_j):
@@ -73,10 +77,10 @@ class Game:
             myplayer=self.secondPlayerValue
             opplayer=self.firstPlayerValue
         myplayer+=self.boardValue[pos_i][pos_j]
-        direction=[(1:1),(1:-1),(-1,1),(-1,-1)]
+        direction=[(0,1),(1,0),(-1,0),(0,-1)]
         raid=[]
         for i in range(4):
-            if opponent(player,pos_i+direction[i][0],pos_j+direction[i][1]):
+            if self.opponent(player,pos_i+direction[i][0],pos_j+direction[i][1]):
                 myplayer+=self.boardValue[pos_i+direction[i][0]][pos_j+direction[i][1]]
                 opplayer-=self.boardValue[pos_i+direction[i][0]][pos_j+direction[i][1]]
                 raid.append(i)
@@ -84,20 +88,27 @@ class Game:
         return (eva,myplayer,opplayer,raid)
 
     def opponent(self,player,i,j):
-        if i>0 and i<5 and j>0 and j<5 and self.boardState[i][j]!="*" and self.boardState[i][j]!=player
+        if i>0 and i<5 and j>0 and j<5 and self.boardState[i][j]!="*" and self.boardState[i][j]!=player:
+            return True
+        return False
     def greedyBFS(self,player,cutoff):
         maxEva=-100000000
         i=-1;j=-1;
         result=()
+        evaResult=()
+        print self.boardState
         for row in range(5):
             for col in range(5):
-                if self.boardState[i][j]=='*'
+                
+                if self.boardState[row][col]=='*':
                     evaResult= self.evaluation(player,row,col)
+                    print evaResult
                     if evaResult[0]>maxEva:
-                        maxEva=cur;i=row;j=col;result=evaResult;
+                        maxEva=evaResult[0];i=row;j=col;result=evaResult;
         if i==-1 and j==-1:
             self.gameEnd=True
-        move(player,evaResult)
+        else:
+            self.move(player,i,j,evaResult)
     def minMax(self,player,cutoff):
         pass
 
@@ -105,9 +116,9 @@ class Game:
         pass
 
     def printState(self):
-        pass
+        print self.boardState
 if __name__=='__main__':
-    if len(sys.argv!=3) or sys.argv[1]!="-i":
+    if len(sys.argv)!=3 or sys.argv[1]!="-i":
         print "input formate error"
         sys.exit()
     game=Game(sys.argv[2])
